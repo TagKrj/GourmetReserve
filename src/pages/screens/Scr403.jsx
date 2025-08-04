86f3d082e89faa95b22f87030ff8f48a1fb5ce0a
TagKrj <trangbui19032004@gmail.com>
2025-08-04 16:30:48 +0700

    feat: enhance Course Availability page with scroll functionality and course data display

--- src/pages/screens/Scr403.jsx
+++ src/pages/screens/Scr403.jsx
@@ -9,6 +9,7 @@ import courseData from '../../constants/courseData';
 
 const CourseAvailabilityPage = () => {
     const fileInputRef = useRef(null);
+    const courseTableScrollRef = useRef(null);
     const [currentDate, setCurrentDate] = useState(new Date());
     const [daysInMonth, setDaysInMonth] = useState([]);
     const [availabilityData, setAvailabilityData] = useState({});
@@ -114,17 +115,44 @@ const CourseAvailabilityPage = () => {
                         </div>
 
                         {/* Course Availability Table */}
-                        <div className="mt-6 overflow-x-auto">
-                            <table className="min-w-full border-collapse">
+                        <div className="flex items-center justify-end space-x-4 mb-5 ">
+                            <div
+                                className='w-8 h-8 flex items-center justify-center rounded-full cursor-pointer border-2 border-gray-200 bg-white hover:bg-gray-100'
+                                onClick={() => {
+                                    if (courseTableScrollRef.current) {
+                                        courseTableScrollRef.current.scrollBy({ left: -200, behavior: 'smooth' });
+                                    }
+                                }}
+                            >
+                                <img src={arrowRight} alt="" style={{
+                                    width: '10px', height: '10px', transform: 'rotate(180deg)'
+                                }} />
+                            </div>
+                            <div
+                                className='w-8 h-8 flex items-center justify-center rounded-full cursor-pointer border-2 border-gray-200 bg-white hover:bg-gray-100'
+                                onClick={() => {
+                                    if (courseTableScrollRef.current) {
+                                        courseTableScrollRef.current.scrollBy({ left: 200, behavior: 'smooth' });
+                                    }
+                                }}
+                            >
+                                <img src={arrowRight} alt="" style={{
+                                    width: '10px', height: '10px', color: '#1A1A1A'
+                                }} />
+                            </div>
+                        </div>
+
+                        <div className=" overflow-x-auto flex border border-gray-200 ">
+                            <table className="min-w-[300px] border-collapse">
                                 <thead>
                                     <tr className="bg-[#F9F8F6]">
-                                        <th className="p-4 border border-gray-200 text-center text-gray-400 font-medium w-24 text-xs">日付</th>
-                                        <th className="p-4 border border-gray-200 text-center text-gray-400 font-medium w-24 text-xs">日付／コース名</th>
-                                        {courseData.courses.map(course => (
+                                        <th className="p-4 border border-gray-200 text-center text-gray-400 font-medium w-24 h-20 text-xs">日付</th>
+                                        <th className="p-4 border border-gray-200 text-center text-gray-400 font-medium w-40 text-xs">日付／コース名</th>
+                                        {/* {courseData.courses.map(course => (
                                             <th key={course.id} className="p-4 border border-gray-200 text-center text-gray-400 font-medium w-32">
                                                 <div className="text-xs line-clamp-2">{course.name}</div>
                                             </th>
-                                        ))}
+                                        ))} */}
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -133,19 +161,19 @@ const CourseAvailabilityPage = () => {
 
                                         return (
                                             <tr key={day.dateString} className={day.isHoliday ? "bg-[rgba(227,90,10,0.1)]" : ""}>
-                                                <td className="p-4 border border-gray-200 text-center">
+                                                <td className="p-4 border border-gray-200 text-center h-15">
                                                     {day.isHoliday && (
                                                         <div className="inline-block px-2 py-0 border-2 border-orange-400 text-orange-500 rounded-md" title={day.isHoliday}>
                                                             祝日
                                                         </div>
                                                     )}
                                                 </td>
-                                                <td className={`p-4 border border-gray-200 text-center font-medium text-gray-500`}>
+                                                <td className={`p-4 border border-gray-200 text-center font-medium text-gray-500 h-15`}>
                                                     {day.date.toLocaleDateString('ja-JP', { month: '2-digit', day: '2-digit' }).replace('/', '/')}
                                                     <span className="ml-1">{['日', '月', '火', '水', '木', '金', '土'][dayOfWeek]}</span>
                                                 </td>
 
-                                                {courseData.courses.map(course => (
+                                                {/* {courseData.courses.map(course => (
                                                     <td key={`${course.id}-${day.dateString}`} className="p-4 border border-gray-200 text-center">
                                                         <label className="inline-flex items-center cursor-pointer">
                                                             <input
@@ -159,17 +187,57 @@ const CourseAvailabilityPage = () => {
                                                             </span>
                                                         </label>
                                                     </td>
-                                                ))}
+                                                ))} */}
                                             </tr>
                                         );
                                     })}
                                 </tbody>
                             </table>
+
+                            <div className='w-full overflow-x-auto scroll-hidden' ref={courseTableScrollRef}>
+                                <table className="min-w-[400px] border-collapse overflow-x-auto">
+                                    <thead>
+                                        <tr className="bg-[#F9F8F6]">
+                                            {courseData.courses.map(course => (
+                                                <th key={course.id} className="p-4 border border-gray-200 text-center text-gray-400 font-medium w-32 h-20">
+                                                    <div className="text-xs line-clamp-2" title={course.name}>{course.name}</div>
+                                                </th>
+                                            ))}
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {daysInMonth.map(day => {
+                                            return (
+                                                <tr key={day.dateString} className={day.isHoliday ? "bg-[rgba(227,90,10,0.1)]" : ""}>
+                                                    {courseData.courses.map(course => (
+                                                        <td key={`${course.id}-${day.dateString}`} className="p-4 border border-gray-200 text-center min-w-[160px] h-15">
+                                                            <label className="inline-flex items-center cursor-pointer">
+                                                                <input
+                                                                    type="checkbox"
+                                                                    className="w-5 h-5 text-[#E35A0A] bg-gray-100 border-gray-300 rounded focus:ring-[#E35A0A] cursor-pointer accent-[#E35A0A]"
+                                                                    checked={isAvailable(course.id, day.dateString)}
+                                                                    onChange={() => toggleAvailability(course.id, day.dateString)}
+                                                                />
+                                                                <span className={`ml-2 font-medium text-gray-400`}>
+                                                                    {isAvailable(course.id, day.dateString) ? 'OK' : 'NG'}
+                                                                </span>
+                                                            </label>
+                                                        </td>
+                                                    ))}
+                                                </tr>
+                                            );
+                                        })}
+                                    </tbody>
+                                </table>
+                            </div>
+
                         </div>
 
                         {/* Save Button */}
-                        <div className="flex justify-end mt-6">
-                            <ButtonPrimary label="保存" onClick={() => alert('予約可能日が保存されました')} />
+                        <div className="flex justify-center mt-6">
+                            <ButtonPrimary >
+                                予約可能日を確定する
+                            </ButtonPrimary>
                         </div>
                     </div>
                 }
